3341ca4e686d36d59864455f00e43e8e1d8151d4
Joey Stilley <Joey.Stilley@gmail.com>
2021-09-20 11:25:58 -0500

    Added appsettings.json file and gitignore statement

---
+++ BestBuyBestPractices/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
+
+namespace BestBuyBestPractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
